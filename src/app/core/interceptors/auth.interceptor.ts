5435e1d070400d86741bb68be399129fc998e4f3
Ajil VC <vcajiltvr@gmail.com>
2025-04-26 17:33:27 +0530

    Authentication all set

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,17 +1,30 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = localStorage.getItem('authToken');
+  const router = inject(Router);
+
+  let cloneReq = req;
   if (token) {
-    const cloneReq = req.clone({
+    cloneReq = req.clone({
       setHeaders : {
         Authorization : `Bearer ${token}`
       }
     });
-
-    return next(cloneReq);
   }
   
-  return next(req);
+  return next(cloneReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if(error.status === 401 || error.status === 403){
+        localStorage.removeItem('authToken');
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  )
+
 };
