e9516479b4a2c1a01b6799224071229f5b617bd0
Ajil VC <vcajiltvr@gmail.com>
2025-09-22 20:44:16 +0530

    Added backend url in environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+const prodBaseUrl = 'https://api.projexpert.xyz/api/v1/';
+
+export const environment = {
+    production: true,
+    apiCompanyUrl: `${prodBaseUrl}company/`,
+    apiUserUrl: `${prodBaseUrl}user/`,
+    apiAdminUrl: `${prodBaseUrl}admin/`,
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 const baseUrl = 'http://localhost:8080/api/v1/';
 const ultraBaseURL = 'http://localhost:8080';
 
+
 export const environment = {
 
     apiCompanyUrl: `${baseUrl}company/`,
