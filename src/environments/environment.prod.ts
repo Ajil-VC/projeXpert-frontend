1fb1712f910c78eaba9a10908f923525dd0b7a6d
Ajil VC <vcajiltvr@gmail.com>
2025-09-22 20:52:02 +0530

    Added ultraBaseUrl in prod environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 const prodBaseUrl = 'https://api.projexpert.xyz/api/v1/';
+const ultraBaseURL = 'https://api.projexpert.xyz';
 
 export const environment = {
     production: true,
     apiCompanyUrl: `${prodBaseUrl}company/`,
     apiUserUrl: `${prodBaseUrl}user/`,
     apiAdminUrl: `${prodBaseUrl}admin/`,
+    ultraBaseURL
 };
