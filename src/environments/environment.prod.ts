bf7a9aaf81db890d4970154b82796b4614ba30e0
Ajil VC <vcajiltvr@gmail.com>
2025-11-05 19:24:29 +0530

    Environment.prod updated with new url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
-const prodBaseUrl = 'https://api.projexpert.xyz/api/v1/';
-const ultraBaseURL = 'https://api.projexpert.xyz';
-
+// const prodBaseUrl = 'https://api.projexpert.xyz/api/v1/';
+// const ultraBaseURL = 'https://api.projexpert.xyz';
+const prodBaseUrl = 'https://projexpert-backend.onrender.com/api/v1/'
+const ultraBaseURL = 'https://projexpert-backend.onrender.com';
 export const environment = {
     production: true,
     apiCompanyUrl: `${prodBaseUrl}company/`,
